a3b70538a84131d6476b44047e432e1bf1ad28a2
Bibek Nayaju <bibeknayaju@github.com>
2022-05-14 23:33:49 +0545

    intialize build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+      {/* global storage as like redux */}
+    </SessionProvider>
+  )
 }
 
 export default MyApp
